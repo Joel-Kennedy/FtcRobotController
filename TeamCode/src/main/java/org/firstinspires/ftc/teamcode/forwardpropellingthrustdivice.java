7aedc85e45764801c6d09edd8d8a8fa5d91994fa
Joel-Kennedy <86170980+Joel-Kennedy@users.noreply.github.com>
2023-10-11 19:51:51 -0600

    removed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/forwardpropellingthrustdivice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/forwardpropellingthrustdivice.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-// blurb
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
