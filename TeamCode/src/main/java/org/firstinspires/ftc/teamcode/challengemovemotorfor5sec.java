ec48163d8b16226f1d527e92d3d5ffa392ba5768
will2729 <147014345+will2729@users.noreply.github.com>
2023-10-11 20:28:54 -0600

    Working On Challenge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/challengemovemotorfor5sec.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/challengemovemotorfor5sec.java
@@ -4,17 +4,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 public class challengemovemotorfor5sec extends LinearOpMode {
     DcMotor Cf;
+     private ElapsedTime   runTime = new ElapsedTime();
     @Override
     public void runOpMode(){
         Cf = hardwareMap.get(DcMotor.class, "ChickenFingers");
         Cf.setDirection(DcMotorSimple.Direction.REVERSE);
         waitForStart();
-        while ( (opModeIsActive())
-        {
+        while (opModeIsActive()){
+            
+        }
 
     }
 }
