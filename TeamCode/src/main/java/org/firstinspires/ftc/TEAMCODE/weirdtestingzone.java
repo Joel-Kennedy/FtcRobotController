8c7c4d1d99577f1cf0bd671a020d34372ea66390
will2729 <147014345+will2729@users.noreply.github.com>
2023-10-30 19:37:06 -0600

    AUTO

--- TeamCode/src/main/java/org/firstinspires/ftc/TEAMCODE/weirdtestingzone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/TEAMCODE/weirdtestingzone.java
@@ -12,9 +12,11 @@ public class weirdtestingzone extends LinearOpMode
     DcMotor frontLeft;
     DcMotor frontRight;
     static final double     FORWARD_SPEED = 0.5;
-    static final double     REVERSE_SPEED = 0.5;
+    static final double     REVERSE_SPEED = -0.5;
     static final double     TURN_SPEED    = 0.5;
+    static final double     TURN_SPEED_RE    = -0.5;// RE = REVERSE
     static final double     CF_speed      = 0.5;
+    static final double     CF_speed_RE      = 0.5;// RE = REVERSE
 
     @Override
     public void runOpMode() {
@@ -24,6 +26,7 @@ public class weirdtestingzone extends LinearOpMode
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        Cf = hardwareMap.get(DcMotor.class, "ChickenFingers");
 //
 //        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
 //        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
@@ -44,18 +47,31 @@ public class weirdtestingzone extends LinearOpMode
         frontRight.setPower(REVERSE_SPEED);
         sleep(1000);
 
-        backLeft.setPower(REVERSE_SPEED);
-        frontLeft.setPower(REVERSE_SPEED);
-        backRight.setPower(FORWARD_SPEED);
-        frontRight.setPower(FORWARD_SPEED);
+
+
+        backLeft.setPower(TURN_SPEED_RE);//LOOK AT TOP FOR DEFINITION OF RE
+        frontLeft.setPower(TURN_SPEED_RE);
+        backRight.setPower(TURN_SPEED);
+        frontRight.setPower(TURN_SPEED);
+        sleep(1000);
+
+        backLeft.setPower(TURN_SPEED);
+        frontLeft.setPower(TURN_SPEED);
+        backRight.setPower(TURN_SPEED_RE);
+        frontRight.setPower(TURN_SPEED_RE);
+        sleep(1000);
+
+        Cf.setPower(CF_speed);
+        sleep(1000);
+
+        Cf.setPower(CF_speed_RE);
         sleep(1000);
 
 
-        backLeft.setPower(FORWARD_SPEED);
-        frontLeft.setPower(FORWARD_SPEED);
-        backRight.setPower(REVERSE_SPEED);
-        frontRight.setPower(REVERSE_SPEED);
-        sleep(1000);
+
+
+
+
 
 
 
